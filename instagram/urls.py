36dcf2eabdc25e979b0ae2922e40d1a6fa1f2284
Hyun Jun Lee <mikey1000@naver.com>
2022-05-26 16:56:07 +0900

    Create upload class

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.Main.as_view())
+    path('', views.Main.as_view()),
+    path('/upload', views.UploadFeed.as_view(), name='upload_feed'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,11 @@
+from django.conf import settings
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .models import Feed
+from rest_framework.response import Response
+import os
+from uuid import uuid4
+
 
 # Create your views here.
 
@@ -8,4 +13,24 @@ class Main(APIView):
     def get(self, request):
         feed_list = Feed.objects.all()
         return render(request, 'instagram/main.html', {'feed_list':feed_list})
+
+
+class UploadFeed(APIView):
+    def post(self, request):
+        file = request.FILES['file']
+        uuid_name = uuid4().hex
+        save_path = os.path.join(settings.MEDIA_ROOT, uuid_name)
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+        content = request.data.get('content')
+        image = uuid_name
+        profile_image = request.data.get('profile_image')
+        user_id = request.data.get('user_id')
+
+        Feed.objects.create(content=content, image=image, profile_image=profile_image, user_id=user_id, like_count=0)
+
+        return Response(status=200)
+
+
 
